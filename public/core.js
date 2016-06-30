fa23113182b8dd03c051102b881945f6ce3fc7fb
Ben <ben@Bens-MacBook.local>
2016-06-30 13:34:52 -0400

    first commit

---
+++ public/core.js
@@ -0,0 +1,40 @@
+var angTodo = angular.module('angTodo', []);
+
+angTodo.controller('mainController', ['$scope', '$http', function ($scope, $http) {
+	
+	$scope.hello = "Hello from controller";
+	
+	$scope.formData = {};
+	
+	$http.get('/api/item')
+		.then(function(data) {
+			$scope.items = data.data;
+			console.log(data);
+		}, 
+		function(data) {
+			console.log('Error: '+data);
+		});
+	
+	$scope.createItem = function() {
+		$http.post('/api/item', $scope.formData)
+			.then(function(data) {
+				$scope.formData = {};
+				$scope.items = data.data;
+				console.log(data);
+			},
+			function(data) {
+				console.log('Error: '+data);
+			});
+	};
+	
+	$scope.deleteItem = function(id) {
+		$http.delete('/api/item/' + id)
+			.then(function (data) {
+				$scope.items = data;
+				console.log(data);
+			},
+			function(data) {
+				console.log('Error: '+ data);
+			});
+	};
+}]);
--- server.js
+++ server.js
@@ -3,9 +3,9 @@ var app = express();
 var mongoose = require('mongoose');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
-var methodOverrride = require('method-override');
+var methodOverride = require('method-override');
 
-mongoURI = //get this from Heroku
+mongoURI = 'mongodb://heroku_mhl9lc6f:nmt4g65blevs8v0d1sbk7hompk@ds023654.mlab.com:23654/heroku_mhl9lc6f';
 
 mongoose.connect(mongoURI);
 
@@ -16,4 +16,75 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({type: 'application/vnd.api+json'}));
 app.use(methodOverride());
 
+var itemSchema = mongoose.Schema({
+	text: String,
+	checked: Boolean
+});
+
+var Item = mongoose.model('item', itemSchema);
+
+app.get('/', function(req, res) {
+	Item.remove({}, function(err, items) {
+		console.log('everything removed');
+		res.send('everything is gone');
+	});
+});
+
+app.get('/api/item', function(req, res) {
+	Item.find({}, function(err, items) {
+		if (err) {
+			res.send(err);
+		} else {
+			console.log(items);
+			res.json(items);
+		}
+ 	});
+});
+
+app.post('/api/item', function (req, res) {
+	Item.create({
+		text: req.body.text,
+		done: false
+	}, function(err, todo) {
+		if (err) {
+			res.send(err);
+		} else {
+			Item.find({}, function(err, items) {
+				if (err) {
+					res.send(err);
+				} else {
+					res.json(items);
+				}
+			});
+		}
+	});
+});
+
+app.delete('/api/item/:todo_id', function(req, res) {
+	console.log(req.params.todo_id);
+	Item.remove({
+			_id: req.params.todo_id
+	}, function(err, item) {
+			if (err) {
+				res.send(err);
+			} else {
+				Item.find({}, function(err, items) {
+					if (err) {
+						res.send(err);
+					} else {
+						res.json(items);
+					}
+				});
+			}
+		});
+});
+
+
+app.get('*', function(req, res) {
+	res.sendFile('./public/index.html');
+});
+
+
+
 app.listen(process.env.PORT || 5000);
+
